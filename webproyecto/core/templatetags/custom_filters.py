e6c9b4da608da315ec41c063680b188b4f0f27aa
Alejandro Avila <alejavieravila93@gmail.com>
2023-09-20 19:17:13 -0300

    agrego url parametrizada para empleados

--- webproyecto/core/templatetags/custom_filters.py
+++ webproyecto/core/templatetags/custom_filters.py
@@ -4,10 +4,12 @@ from datetime import datetime
 register = template.Library()
 
 @register.filter
-def format_date(value, format_string="%d-%m-%Y %H:%M:%S"):
+def format_date(value):
+    #format_string="%d-%m-%Y %H:%M:%S"
+    format_string="%Y-%m-%d"
     if value:
         if isinstance(value, str):
-            value = datetime.strptime(value, "%d-%m-%Y %H:%M:%S")
+            value = datetime.strptime(value, format_string)
         return value.strftime(format_string)
     return ""
 
