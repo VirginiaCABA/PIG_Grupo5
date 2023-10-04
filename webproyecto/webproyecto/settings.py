06905ace682ddfd7a6fb2fc8fa1500b596ca6431
Alejandro Avila <alejavieravila93@gmail.com>
2023-10-04 19:20:46 -0300

    ajustes de configuracion
    reemplazo psycopg2 por psycopg2-binary
    
    utilizo config de decouple para esconder claves en archivo .env

--- webproyecto/webproyecto/settings.py
+++ webproyecto/webproyecto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cy@v%)nj-qdvjnfzy+h+&efd0!l0f65qr*rh#wna4zu^51j-0h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,23 +75,16 @@ WSGI_APPLICATION = 'webproyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'django2023',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
         'OPTIONS': {
-            'options': '-c search_path=django',
+            'options': '-c search_path='+config("DATABASE_SCHEMA"),
         },
     }
 }
