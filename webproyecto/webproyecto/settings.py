75229aa8b44772b5976512f2f5203dc79a1ccbd4
Alejandro Avila <alejavieravila93@gmail.com>
2023-10-04 00:41:16 -0300

    agrego config de conexión a postgresql

--- webproyecto/webproyecto/settings.py
+++ webproyecto/webproyecto/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'webproyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
