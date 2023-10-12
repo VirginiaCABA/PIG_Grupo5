fbd7459b9dfffaa301949ec2c1629178ec5e53bd
omardila21 <130505448+omardila21@users.noreply.github.com>
2023-10-12 08:20:24 -0300

    models

--- webproyecto/core/models.py
+++ webproyecto/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+#create table
+class User(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=10, blank=True, null=True)
+    lastname = models.CharField(max_length=100, blank=True, null=True) 
+    dni =  models.CharField(max_length=10, validators=[RegexValidator(r'^\d{8,10}$', message='El número de dni debe tener entre 8 y 10 dígitos.')])
+    mail = models.EmailField(max_length=100, unique=True)
+    curriculum = models.FileField(upload_to='cv_upload/')
+    mensaje = models.TextField(max_length=500, blank=True, null=True)
+    class Meta:
+            app_label = 'core'
--- webproyecto/webproyecto/settings.py
+++ webproyecto/webproyecto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
